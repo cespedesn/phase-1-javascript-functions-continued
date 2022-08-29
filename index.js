f9c8d5d67c230fbe5acb4fb7ead68dbba438c4ee
Neftali Cespedes <cesinvestmentholdings@gmail.com>
2022-08-29 19:27:53 -0400

    commit tree 7a4b7bcd139bc2fbbb62983b79e9b69a7ce777ac

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork();
+
+
+function wrapAdjective(attribute = "*") {
+    return function () {
+        return `You are ${attribute}a hard worker${attribute}!`;
+        } 
+}
+wrapAdjective("You are ||a dedicated programmer||!");
